f2c8f611564ce29821bb6b0a1091d5d837143630
Leonell Cruz <102971421+leonellcruz7@users.noreply.github.com>
2023-09-18 16:38:13 +0800

    create ui structure for trend list section

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["res.cloudinary.com"], // Add the domains from which your images will be served
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
