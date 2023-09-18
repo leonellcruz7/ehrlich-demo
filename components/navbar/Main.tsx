78ce1ca78ece5ac4bd455f7dab3ea2e6b0767dbf
Leonell Cruz <102971421+leonellcruz7@users.noreply.github.com>
2023-09-18 14:34:12 +0800

    create secondary navbar

--- components/navbar/Main.tsx
+++ components/navbar/Main.tsx
@@ -2,32 +2,44 @@ import React from "react";
 import styles from "./styles.module.scss";
 import Logo from "../assets/Logo";
 import CurrencySelector from "./CurrencySelector";
+import classNames from "classnames";
+import Link from "next/link";
 
 const Main = () => {
-  const menus = ["women", "plus", "men", "accessories"];
+  const menus = [
+    { title: "women", link: "#" },
+    { title: "plus", link: "#" },
+    { title: "men", link: "#" },
+    { title: "accessories", link: "#" },
+  ];
 
   return (
-    <div className={styles.main}>
-      <ul className={styles.menuList}>
-        {menus.map((menu, index) => {
-          return (
-            <li className={styles.menu} key={index}>
-              <p>{menu}</p>
-            </li>
-          );
-        })}
-      </ul>
-      <div>
-        <Logo />
-      </div>
-      <div>
-        <div className={styles.navbarFeatures}>
-          <i className="ri-user-line"></i>
-          <i className="ri-shopping-bag-line"></i>
-          <i className="ri-heart-line"></i>
-          <i className="ri-customer-service-fill"></i>
-          <i className="ri-search-line"></i>
-          <CurrencySelector />
+    <div>
+      <div className={classNames("main-container", styles.main)}>
+        <ul className={styles.menuList}>
+          {menus.map((menu, index) => {
+            const { title, link } = menu;
+            return (
+              <Link className={styles.menu} href={link} key={index}>
+                <li>{title}</li>
+              </Link>
+            );
+          })}
+        </ul>
+        <div>
+          <Link href="#">
+            <Logo />
+          </Link>
+        </div>
+        <div>
+          <div className={styles.navbarFeatures}>
+            <i className="ri-user-line"></i>
+            <i className="ri-shopping-bag-line"></i>
+            <i className="ri-heart-line"></i>
+            <i className="ri-customer-service-fill"></i>
+            <i className="ri-search-line"></i>
+            <CurrencySelector />
+          </div>
         </div>
       </div>
     </div>
