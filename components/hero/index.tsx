71276edac47a8f8f7a989b655d18976542d1aa82
Leonell Cruz <102971421+leonellcruz7@users.noreply.github.com>
2023-09-19 01:07:33 +0800

    change promo image file name

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./styles.module.scss";
 import Image from "next/image";
 import hero from "@/public/hero.png";
-import promo from "@/public/hero 1.png";
+import promo from "@/public/promo.png";
 import background from "@/public/Rectangle.png";
 import Deals from "./Deals";
 
@@ -18,13 +18,7 @@ const Hero = () => {
         </div>
         <div className="z-50 flex flex-col items-center">
           <div className={styles.promo}>
-            <Image
-              alt=""
-              width={644}
-              height={446}
-              src={promo}
-              className="w-full"
-            />
+            <Image alt="" width={644} height={446} src={promo} />
           </div>
           <button className="button white large mx-auto">shop now</button>
         </div>
